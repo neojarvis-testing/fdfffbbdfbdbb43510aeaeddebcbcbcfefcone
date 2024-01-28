46085abe137af26c74d07f3a044019fb5abe7341
neojarvis <techadmin@iamneo.ai>
2024-01-28 17:09:17 +0000

    Auto-save-commit-28/01/2024--22:39:12

--- main.sql
+++ main.sql
@@ -9,4 +9,3 @@
     INSERT INTO ExampleTable (ID, Name) VALUES (2, 'Bob');
 
 -- Select and view all data from the tabl
-sqlcmd -S 127.0.0.1 -d loki -U SA -P examlyMssql@123 -i /home/coder/project/workspace/main.sql
